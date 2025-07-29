c86ae0fe12d72a1104bdebd0d6ed817006ff76d8
HaiderMarhoon <haidooer2003@gmail.com>
2025-07-29 12:52:44 +0300

    Fix Heroku crash: added Procfile and ensured env vars

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.use((err, req, res, next) => {
   })
 })
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
